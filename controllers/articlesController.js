480ee7e83cc699722be6293468456d131fe9ea92
James Milmine <jamesmilmine@mac.com>
2023-02-20 15:24:39 +0000

    update articlesController with catch

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,6 +1,6 @@
 const { fetchArticles } = require("../models/articlesModels");
 
-exports.getArticles = (error, request, response, next) => {
+exports.getArticles = (request, response, next) => {
 	return fetchArticles()
 		.then((articles) => {
 			response.status(200).send({ articles });
