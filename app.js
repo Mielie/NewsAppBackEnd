225d58c7728022727d6fcb596063b2b32f48be61
James Milmine <jamesmilmine@mac.com>
2023-02-20 14:42:38 +0000

    remove unnecessary express middleware and update errorhandler to deal with 500 errors

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const { customErrorHandler } = require("./controllers/errorHandlerControllers");
 
 const app = express();
 
-app.use(express.json());
-
 app.get("/api/topics", getTopics);
 
 app.use(customErrorHandler);
